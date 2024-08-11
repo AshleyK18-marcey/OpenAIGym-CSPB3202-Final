f15dd4944bc3494ac30e0917d231ea70c0948037
AshleyK18-marcey <akastler18@gmail.com>
2024-08-10 20:08:38 -0600

    add step count and adjust learning and discount factors

--- mountain_car_v0.py
+++ mountain_car_v0.py
@@ -2,13 +2,16 @@ import gymnasium as gym
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+
 # Q learning algorithm
 # Parameters: 
 # episodes - the number of 200 step episodes you want to train the agent for
+# alpha - learning rate 
+# gamma - Discount factor
 # training - if true then the q table is reset and the agent is trained, 
 # if false then the agent will use the saved q table to complete the control problem
 # render - if true then pygame will render the env, if false then it will run without a visualization (saves time)
-def q_train(episodes, training=True, render=False):
+def q_train(episodes, alpha = 0.1, gamma = 0.99, training=True, render=False):
     # Initialize the mountain car environment
     env = gym.make("MountainCar-v0", render_mode="human" if render else None)
 
@@ -24,10 +27,6 @@ def q_train(episodes, training=True, render=False):
         q_table = pickle.load(file)
         file.close()
 
-    # Set learning parameters (I found these were the best)
-    alpha = 0.9 # Learning rate 
-    gamma = 0.9 # Discount factor
-
     # Epsilon-greedy parameters
     epsilon = 1 # Initial exploration rate, 100% random actions at first
     epsilon_decay = 2/episodes # Epsilon decay rate
@@ -36,6 +35,7 @@ def q_train(episodes, training=True, render=False):
     # Initialize arrays to track rewards and successful episodes
     rewards_episode = np.zeros(episodes)
     successful_episode = np.zeros(episodes)
+    steps_per_episode = np.zeros(episodes)
 
     for i in range(episodes):
         # Reset the environment to start a new episode
@@ -47,10 +47,9 @@ def q_train(episodes, training=True, render=False):
 
         terminated = False # If goal is reached then this will be true
         rewards = 0
-
         # capped the penalty of rewards to allow for neater plotting
         while(not terminated and rewards > -1000):
-            
+
             # Determine action using epsilon greedy strategy
             if training and rng.random() < epsilon:
                 # take random action (0 = left, 1= do nothing, 2= right)
@@ -63,12 +62,14 @@ def q_train(episodes, training=True, render=False):
 
             # Take action
             new_state, reward, terminated, _, _ = env.step(action)
+            steps_per_episode[i] += 1
             
             # Discretize the new state
             new_state_p = np.digitize(new_state[0], pos_space)
             new_state_v = np.digitize(new_state[1], vel_space)
 
             if training:
+                # Update the Q table
                 q_table[state_p, state_v, action] = q_table[state_p, state_v, action] + alpha * (
                     reward + gamma * np.max(q_table[new_state_p,new_state_v, :]) - q_table[state_p, state_v, action]
                 )
@@ -76,6 +77,7 @@ def q_train(episodes, training=True, render=False):
             # Check if the car has reached the goal
             if new_state[0] >= 0.5:
                 print('Success on episode {}'.format(i+1))
+                print('Success after {} steps'.format(steps_per_episode[i]))
                 successful_episode[i] = 1
             
             # Update state and rewards for the next step
@@ -83,6 +85,7 @@ def q_train(episodes, training=True, render=False):
             state_p = new_state_p
             state_v = new_state_v
             rewards+=reward
+
         # Decay epsilon to reduce exploration over time
         epsilon = max(epsilon - epsilon_decay, 0)
         # Track total rewards for the episode
@@ -104,11 +107,12 @@ def q_train(episodes, training=True, render=False):
     
     # Highlight the successful episodes on the plot
     highlight_indices = np.where(successful_episode == 1)[0]
-    plt.scatter(highlight_indices, mean_rewards[highlight_indices], color='red', zorder=5, label='Successful Episodes')
+    plt.scatter(highlight_indices, mean_rewards[highlight_indices], color='green', label='Successful Episodes', linewidths=0.1)
     
     plt.title('Average Rewards vs Episodes')
     plt.xlabel('Episodes')
     plt.ylabel('Average Rewards')
     plt.savefig(f'moun.png')
 
-q_train(1000, True, False)
+# Call the Q-Learning algorithm
+q_train(10000, 0.1, 0.99, True, False)
